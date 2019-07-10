b3643e073dedefdf783e41fa856320f2e528ef03
KimDaeyong <dev.kimdaeyong@gmail.com>
2019-07-11 02:59:26 +0900

    user의 pillInfo를 pillHistories 로 변경

--- lib/models/User.dart
+++ lib/models/User.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+
+import 'PillHistory.dart';
 import 'PillInfo.dart';
 
 class User {
@@ -5,7 +8,7 @@ class User {
   String name;
   String code;
   String profileImageUri;
-  PillInfo pillInfo;
+  List<PillHistory> pillHistories;
   List<String> friends;
   void makedata(){
     this.name = "김동현";
@@ -17,4 +20,45 @@ class User {
     this.code = "1a2s3d";
     this.profileImageUri = "https://sample-videos.com/img/Sample-jpg-image-500kb.jpg";
   }
+}
+
+User dummyUser() {
+  var user = User();
+  user.name = "김대용";
+  user.code = "2Dqi8Ww";
+  user.profileImageUri = "https://sample-videos.com/img/Sample-jpg-image-500kb.jpg";
+
+  var pillInfo1 = PillInfo();
+  pillInfo1.pillCount = 5;
+  pillInfo1.key = 1;
+  pillInfo1.remainingPills = 5;
+  pillInfo1.time = TimeOfDay.now();
+
+  var pillInfo2 = PillInfo();
+  pillInfo2.pillCount = 1;
+  pillInfo2.key = 2;
+  pillInfo2.remainingPills = 4;
+  pillInfo2.time = TimeOfDay.now();
+
+  var pillInfo3 = PillInfo();
+  pillInfo3.pillCount = 2;
+  pillInfo3.key = 3;
+  pillInfo3.remainingPills = 2;
+  pillInfo3.time = TimeOfDay.now();
+  
+  var history = PillHistory();
+  history.morning = pillInfo1;
+  history.morningStatus = true;
+
+  history.lunch = pillInfo2;
+  history.lunchStatus = false;
+  
+  history.dinner = pillInfo3;
+  history.dinnerStatus = false;
+  
+  history.date = DateTime.now();
+  
+  user.pillHistories = [history];
+  
+  return user;
 }
