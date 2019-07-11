d61a0d16f7061fe0fd9c000f8faab413175d23f7
ByungHyen <xeken@naver.com>
2019-07-11 21:19:50 +0900

    복약정보 완성

--- lib/pages/MyPill.dart
+++ lib/pages/MyPill.dart
@@ -3,7 +3,6 @@ import 'package:pillwhen/behaviors/emptyScrollBehavior.dart';
 import 'package:pillwhen/models/PillInfo.dart';
 import 'package:pillwhen/widgets/MyPillItem.dart';
 import 'package:pillwhen/widgets/card.dart';
-import 'package:pillwhen/widgets/homeItem.dart';
 
 import 'MyPillAddInfo.dart';
 
@@ -13,85 +12,62 @@ class MyPill extends StatefulWidget {
 }
 
 class _MyPillState extends State<MyPill> {
-  //var a = new PillInfo("아침 감기약", new DateTime(2019, 7,10), 3);
-  //var b = new PillInfo("점심 영양제쨩", new DateTime(2019, 7,9), 6);
-  //var c = new PillInfo("저녁 감기약", new DateTime(2019, 7,10), 3);
-//  var pillInfo1 = new PillInfo();
-//  pillInfo1.key = 1;;
-//  pillInfo1.remainingPills = 5;
-//  pillInfo1.time = TimeOfDay.now();
-  var pillInfoList = <PillInfo>[];
+  var pillInfoList = Set<PillInfo>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
         children: <Widget>[
-          HomeItem(
-            title: '내가 먹는 약',
-            child: SizedBox(
-              height: 200,
-              child: ScrollConfiguration(
-                behavior: EmptyScrollBehavior(),
-                child: ListView(
+          SizedBox(
+            height: 200,
+            child: ScrollConfiguration(
+              behavior: EmptyScrollBehavior(),
+              child: ListView(
                   scrollDirection: Axis.vertical,
                   padding:
                       EdgeInsets.symmetric(horizontal: 30.0, vertical: 30.0)
-                          .copyWith(bottom: 25.0),
-                  children: <Widget>[
-                    PwCard(
-                      child: MyPillItem(
-                        nickname: "아침 감기약",
-                        pillNum: 3,
-                        takingTime: new DateTime(2019, 7, 10),
-                      ),
-                      onTap: () {
-                        Navigator.of(context)
-                            .push(MaterialPageRoute(builder: (context) {
-                          var a = new PillInfo();
-                          a.nickName = "아침 감기약";
-                          a.pillCount = 3;
-                          a.dateTime = new DateTime(2019, 7, 10);
-                          pillInfoList.add(a);
-                          print("asdasds" +pillInfoList.toString());
-                          return MyPillAddInfo(pillInfo: pillInfoList.elementAt(0));
-                        }));
-                      },
-                    ),
-                    Padding(padding: EdgeInsets.only(right: 10.0)),
-                    PwCard(
-                      child: Text('ㅁㄴㅇㄴ루ㅏㅁㄴ루ㅏㄴㅁㄹ'),
-                      onTap: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => MyPillAddInfo(
-                                pillInfo: pillInfoList.elementAt(1))));
-                      },
-                    ),
-                    Padding(padding: EdgeInsets.only(right: 10.0)),
-                    PwCard(
-                      child: Text('ㄴ윌뮈프ㅐㅔㄷ믐ㅍ페프ㅐ페ㅔ'),
-                      onTap: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => MyPillAddInfo(
-                                pillInfo: pillInfoList.elementAt(2))));
-                      },
-                    ),
-                  ],
-                ),
-              ),
+                          .copyWith(bottom: 50.0),
+                  children: _buildInfo(pillInfoList)),
             ),
-            badgeText: "2",
           ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.of(context).push(MaterialPageRoute(
-              builder: (context) => MyPillAddInfo(pillInfoList: pillInfoList)));
+              builder: (context) => MyPillAddInfo(
+                  btnText: "추가하기",
+                  pillInfo: PillInfo(),
+                  pillInfoList: pillInfoList)));
         },
         child: Icon(Icons.add),
         elevation: 2.0,
       ),
     );
+  }
+
+  List<Widget> _buildInfo(Set<PillInfo> list) {
+    return list.map((e) {
+      return Padding(
+          padding: EdgeInsets.only(right: 10.0),
+          child: PwCard(
+              child: MyPillItem(
+                nickname: e.nickName,
+                pillNum: e.pillCount,
+                takingTime: e.dateTime,
+              ),
+              onTap: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) {
+                  var a = new PillInfo();
+                  a.nickName = e.nickName;
+                  a.pillCount = e.pillCount;
+                  a.dateTime = e.dateTime;
+                  pillInfoList.add(a);
+                  return MyPillAddInfo(pillInfo: e);
+                }));
+              }));
+    }).toList();
   }
 }
--- lib/pages/MyPillAddInfo.dart
+++ lib/pages/MyPillAddInfo.dart
@@ -1,33 +1,116 @@
 import 'package:flutter/material.dart';
 import 'package:pillwhen/models/PillInfo.dart';
+import 'package:flutter_cupertino_date_picker/flutter_cupertino_date_picker.dart';
+import 'package:intl/intl.dart';
 
 class MyPillAddInfo extends StatefulWidget {
   final PillInfo pillInfo;
-  final List<PillInfo> pillInfoList;
+  final Set<PillInfo> pillInfoList;
+  final String btnText;
 
-  const MyPillAddInfo({Key key, this.pillInfo, this.pillInfoList})
-      : super(key: key);
+  const MyPillAddInfo({Key key, this.pillInfo, this.pillInfoList, this.btnText}) : super(key: key);
+
 
   @override
   _MyPillAddInfoState createState() => _MyPillAddInfoState();
 }
 
 class _MyPillAddInfoState extends State<MyPillAddInfo> {
+
+  TextEditingController nickNameController;
+  TextEditingController countController;
+
+  DateTimePickerLocale _locale = DateTimePickerLocale.en_us;
+  DateTime _dateTime;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _dateTime = DateTime.now();
+
+    nickNameController = TextEditingController(text: widget.pillInfo.nickName);
+    countController = TextEditingController(text: widget.pillInfo.pillCount != null ? widget.pillInfo.pillCount.toString() : '');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 30.0).copyWith(top: 50.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            Text('복약 정보 관리', style: Theme.of(context).textTheme.title,),
+            Padding(padding: EdgeInsets.only(bottom: 10.0),),
             TextField(
-              decoration: InputDecoration(hintText: '별명'),
+              controller: nickNameController,
+              decoration: InputDecoration(hintText: widget.pillInfo.nickName ?? "약 별명"),
             ),
+            Padding(padding: EdgeInsets.only(bottom: 10.0),),
             TextField(
-              decoration: InputDecoration(hintText: '약 갯수'),
+              controller: countController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(hintText: "약 갯수"),
+
             ),
-            TextField(
-              decoration: InputDecoration(hintText: '시간'),
-            )
+            Padding(padding: EdgeInsets.only(bottom: 20.0),),
+            Row(
+              children: <Widget>[
+                FloatingActionButton(
+                  onPressed: (){
+                    DatePicker.showDatePicker(
+                      context,
+                      pickerTheme: DateTimePickerTheme(
+                        showTitle: true,
+                        confirm: Text('Done', style: TextStyle(color: Colors.cyan)),
+                        cancel: Text('Cancel', style: TextStyle(color: Colors.red)),
+                      ),
+                      pickerMode: DateTimePickerMode.time,
+                      initialDateTime: _dateTime,
+                      locale: _locale,
+                      onCancel: () {
+                        debugPrint('onCancel');
+                      },
+                      onChange: (dateTime, List<int> index) {
+                        setState(() {
+                          _dateTime = dateTime;
+                        });
+                      },
+                      onConfirm: (dateTime, List<int> index) {
+                        setState(() {
+                          _dateTime = dateTime;
+                        });
+                      },
+                    );
+                  },
+                  elevation: 6.0,
+                  child: Icon(Icons.access_time),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15.0),
+                  child: Text(_dateTime.toString()),
+                ),
+              ],
+            ),
+            Expanded(child: Container()),
+            RaisedButton(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10.0))),
+              onPressed: () {
+                var pillInfo = new PillInfo();
+                pillInfo.nickName = nickNameController.text;
+                pillInfo.pillCount = int.parse(countController.text);
+                pillInfo.dateTime = _dateTime;
+                widget.pillInfoList.add(pillInfo);
+                Navigator.pop(context);
+              },
+              elevation: 8.0,
+              color: Colors.red,
+              textColor: Colors.white,
+              child: Text(widget.btnText ?? "수정하기"),
+              padding: EdgeInsets.symmetric(vertical: 12.5)
+            ),
+            Padding(padding: EdgeInsets.only(bottom: 30.0))
           ],
         ),
       ),
